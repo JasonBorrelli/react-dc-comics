4cbf1c1f402e73e1d7212813a3722adafc1530a2
JasonBorrelli <borrellijason@gmail.com>
2026-09-20 19:19:00 +0200

    feat: add Footer, MainContent, and ProductList components

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,8 +1,18 @@
 export default function Footer() {
     return (
         <footer>
-            <p>Footer</p>
+            <div className="bg-zinc-800 flex justify-between p-5 mt-5">
+                <button className="text-white bg-transparent border border-blue-500 px-3 py-2 align-center">SIGN-UP NOW!</button>
+                <p className="text-blue-500 text-lg  font-bold bg-transparent px-3 py-2 align-center">FOLLOW US</p>
+                <ul className="flex space-between gap-5 justify-center align-center">
+                    <img className="w-10 h-10" src="src/assets/img/footer-facebook.png" alt="" />
+                    <img className="w-10 h-10" src="src/assets/img/footer-twitter.png" alt="" />
+                    <img className="w-10 h-10" src="src/assets/img/footer-youtube.png" alt="" />
+                    <img className="w-10 h-10" src="src/assets/img/footer-pinterest.png" alt="" />
+                    <img className="w-10 h-10" src="src/assets/img/footer-periscope.png" alt="" />
+                </ul>
 
+            </div>
         </footer>
     )
 }
--- src/components/layout/MainContent.jsx
+++ src/components/layout/MainContent.jsx
@@ -1,7 +1,7 @@
 export default function MainContent() {
     return (
         <main>
-
+            <h1>COMICS</h1>
         </main>
     )
 }
