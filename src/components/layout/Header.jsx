e64ea6a336948bf388734e0429631df6480f1118
JasonBorrelli <borrellijason@gmail.com>
2026-09-20 23:12:12 +0200

    feat: fixed header css

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -6,16 +6,16 @@ export default function Header() {
             <div className="flex flex-col lg:flex-row justify-between items-center container mx-auto mt-5 mb-5 px-4">
                 <Logo />
                 <ul className="flex flex-wrap justify-center lg:justify-end gap-5 bg-white text-black p-4 lg:p-8 uppercase font-bold text-xs">
-                    <span className="hover:text-blue-500">Characters</span>
-                    <span className="hover:text-blue-500">Comics</span>
-                    <span className="hover:text-blue-500">Movies</span>
-                    <span className="hover:text-blue-500">TV</span>
-                    <span className="hover:text-blue-500">Games</span>
-                    <span className="hover:text-blue-500">Collectibles</span>
-                    <span className="hover:text-blue-500">Videos</span>
-                    <span className="hover:text-blue-500">Fans</span>
-                    <span className="hover:text-blue-500">News</span>
-                    <span className="hover:text-blue-500">Shop</span>
+                    <a href="#" className="hover:text-blue-500 cursor-pointer">Characters</a>
+                    <a href="#" className="hover:text-blue-500 cursor-pointer">Comics</a>
+                    <a href="#" className="hover:text-blue-500 cursor-pointer">Movies</a>
+                    <a href="#" className="hover:text-blue-500 cursor-pointer">TV</a>
+                    <a href="#" className="hover:text-blue-500 cursor-pointer">Games</a>
+                    <a href="#" className="hover:text-blue-500 cursor-pointer">Collectibles</a>
+                    <a href="#" className="hover:text-blue-500 cursor-pointer">Videos</a>
+                    <a href="#" className="hover:text-blue-500 cursor-pointer">Fans</a>
+                    <a href="#" className="hover:text-blue-500 cursor-pointer">News</a>
+                    <a href="#" className="hover:text-blue-500 cursor-pointer">Shop</a>
                 </ul>
             </div>
         </header>
